11acb64296f5a64bbfd4fb9b5d9d77b24dac0ade
Victor Otieno <victorotieno598@gmail.com>
2021-09-14 13:04:16 +0300

    Add class to App.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React, { Component } from 'react'
 import Calculator from "./components/Calculator";
 
-function App() {
-  return (
-    <div className="container">
-      <Calculator />
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Calculator />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
