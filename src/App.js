ae02114f52b2e4374ef63fa8bd440664c86b3ca2
Victor Otieno <victorotieno598@gmail.com>
2021-09-14 13:21:50 +0300

    Fix linters

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Calculator from "./components/Calculator";
 
 export class App extends Component {
   render() {
     return (
-      <div className="container">
+      <div className='container'>
         <Calculator />
       </div>
     )
   }
 }
 
-export default App
-
+export default App;
--- src/components/Numpad.js
+++ src/components/Numpad.js
@@ -9,30 +9,30 @@ export class Numpad extends Component {
                     <td>AC</td>
                     <td>+/-</td>
                     <td>%</td>
-                    <td className="operator">&divide;</td>
+                    <td className='operator'>&divide;</td>
                 </tr>
                 <tr>
                     <td>7</td>
                     <td>8</td>
                     <td>9</td>
-                    <td className="operator">x</td>
+                    <td className='operator'>x</td>
                 </tr>
                 <tr>
                     <td>4</td>
                     <td>5</td>
                     <td>6</td>
-                    <td className="operator">-</td>
+                    <td className='operator'>-</td>
                 </tr>
                 <tr>
                     <td>1</td>
                     <td>2</td>
                     <td>3</td>
-                    <td className="operator">+</td>
+                    <td className='operator'>+</td>
                 </tr>
                 <tr>
-                    <td colspan="2">0</td>
+                    <td colspan='2'>0</td>
                     <td>.</td>
-                    <td className="operator">=</td>
+                    <td className='operator'>=</td>
                 </tr>
           </table> 
         </div>
