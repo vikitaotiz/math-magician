172ce9fcc603f9972939a88cead33a1464a2abdd
Victor Otieno <victorotieno598@gmail.com>
2021-09-14 14:06:53 +0300

    Fix linters

--- src/components/Numpad.js
+++ src/components/Numpad.js
@@ -1,48 +1,47 @@
 import React, { Component } from 'react';
 
 class Numpad extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    
-    render() {
-        return (
-            <div>
-                <table>
-                    <tr>
-                        <td>AC</td>
-                        <td>+/-</td>
-                        <td>%</td>
-                        <td className='operator'>&divide;</td>
-                    </tr>
-                    <tr>
-                        <td>7</td>
-                        <td>8</td>
-                        <td>9</td>
-                        <td className='operator'>x</td>
-                    </tr>
-                    <tr>
-                        <td>4</td>
-                        <td>5</td>
-                        <td>6</td>
-                        <td className='operator'>-</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td className='operator'>+</td>
-                    </tr>
-                    <tr>
-                        <td colspan='2'>0</td>
-                        <td>.</td>
-                        <td className='operator'>=</td>
-                    </tr>
-                </table>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <table>
+          <tr>
+            <td>AC</td>
+            <td>+/-</td>
+            <td>%</td>
+            <td className='operator'>&divide;</td>
+          </tr>
+          <tr>
+            <td>7</td>
+            <td>8</td>
+            <td>9</td>
+              <td className='operator'>x</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+            <td className='operator'>-</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td className='operator'>+</td>
+          </tr>
+          <tr>
+            <td colspan='2'>0</td>
+            <td>.</td>
+            <td className='operator'>=</td>
+          </tr>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default Numpad
--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 
 class Screen extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    
-    render() {
-        return (
-            <div className='screen'>
-                0
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className='screen'>
+          0
+      </div>
+    );
+  }
 }
 
 export default Screen;
