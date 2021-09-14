dd55c24192e36ac3d41e15b7d637aa0c68ade4ed
Victor Otieno <victorotieno598@gmail.com>
2021-09-14 14:00:15 +0300

    Fix linters

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,19 +3,18 @@ import Screen from './Screen';
 import Numpad from './Numpad';
 
 class Calculator extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    
-    render() {
-        return (
-            <div>
-                <Screen />
-                <Numpad />
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <Screen />
+        <Numpad />
+      </div>
+    );
+  }
 }
 
 export default Calculator;
