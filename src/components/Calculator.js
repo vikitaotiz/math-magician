03a9d05773ad1d0beeb44c2a44defab2bf9dd35f
Victor Otieno <victorotieno598@gmail.com>
2021-09-15 14:51:38 +0300

    Fix logic

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
-import Screen from './Screen';
-import Numpad from './Numpad';
-import calculate from '../logic/calculate';
-
+import Screen from './Screen.js';
+import Numpad from './Numpad.js';
+import calculate from '../logic/calculate.js';
+    
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-  }
+      this.state = {
+        total: 0,
+        next: null,
+        operation: null,
+      };
+    }
 
-  onKeyPress = (data) => {
-    this.setState((obj) => calculate(obj, data));
-  }
-
-  render() {
-    return (
-      <div>
-        <Screen total={this.state.total} operation={this.state.operation} next={this.state.next}/>
-        <Numpad onKeyPress={this.onKeyPress} />
-      </div>
-    );
-  }
+    onKeyPress = (data) => {
+      this.setState((obj) => calculate(obj, data));
+    }
+  
+    render() {
+      return (
+        <div>
+          <Screen total={this.state.total} operation={this.state.operation} next={this.state.next}/>
+          <Numpad onKeyPress={this.onKeyPress} />
+        </div>
+      );
+    }
 }
 
-export default Calculator;
+export default Calculator
