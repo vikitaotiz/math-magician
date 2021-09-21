a0467b4ad3edc9458a01875a607dc08c3ae4077c
Victor Otieno <victorotieno598@gmail.com>
2021-09-21 19:08:31 +0300

    Fix linter

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -15,17 +15,7 @@ const Calculator = () => {
       <div>
         <h3>Calculator</h3>
         <hr />
-        <p className="wrap">
-          Lorem Ipsum is simply dummy text of the printing and
-          typesetting industry. Lorem Ipsum has been the industry's
-          standard dummy text ever since the 1500s, when an unknown printer
-          took a galley of type and scrambled it to make a type specimen book.
-          It has survived not only five centuries, but also the leap into electronic
-          typesetting, remaining essentially unchanged. It was popularised in the
-          1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-          and more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
+        <p className="wrap">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
       </div>
       <div>
         <Screen total={obj.total} operation={obj.operation} next={obj.next} />
